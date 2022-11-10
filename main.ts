806608b2e6620b1ccc5c7dfff8df8fdc424fab30
amauryavila <100004825+amauryavila@users.noreply.github.com>
2022-11-10 13:24:27 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showNumber(randint(1, 6))
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Heart)
+})
